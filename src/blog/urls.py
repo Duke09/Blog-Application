9488a6dfaeb85deb7f7306d910b967dd947b21e0
Duke09 <debopriyo09@outlook.com>
2020-06-26 13:33:06 +0530

    Views , urls, and canonical urls

---
+++ src/blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import *
+
+app_name = 'blog'
+
+urlpatterns = [
+    #post views
+    path('', post_list, name='post_list'),
+    path(
+        '<int:year>/<int:month>/<int:day>/<slug:post>/',
+        post_detail, name='post_detail'
+    )
+]
